1778d65e825753a76e7ee148fc907e95a7ce6554
John <john.uko@live.com>
2022-07-01 21:27:08 -0700

    Three digit combo program

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Print combination of 3 digits
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = 1; b <= 8; b++)
+		{
+			if (a < b)
+			{
+				for (c = 2; c <= 9; c++)
+				{
+					if (b < c)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(c + '0');
+						if (a != 7 || b != 8 || c != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
