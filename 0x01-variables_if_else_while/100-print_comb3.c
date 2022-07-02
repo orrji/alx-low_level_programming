8f49ec5493e2adf263518ed0b46de63ad8fec6e9
John <john.uko@live.com>
2022-07-01 20:58:58 -0700

    Two digit program1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,17 @@
 
 int main(void)
 {
-	int a;
-	int b;
+	int a, b;
 
-	for (a = '0'; a <= '8'; a++)
+	for (a = 0; a <= 8; a++)
 	{
-		for (b = '1'; b <= '9'; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			if (a < b)
 			{
 				putchar(a);
 				putchar(b);
-				if (a = '8')
+				if (a != 8 || b != 9)
 				{
 					putchar(',');
 					putchar(' ');
