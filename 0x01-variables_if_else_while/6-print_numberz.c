cdedc1674c0941181c597afd0e87c358fdbea145
John <john.uko@live.com>
2022-07-01 18:54:03 -0700

    Print numberz program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digit numbers of base 10 without using char
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return(0);
+}
