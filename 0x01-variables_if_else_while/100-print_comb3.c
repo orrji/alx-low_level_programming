3b256cdfe1cfa24b34ea4cd6605a28b8fd8668c1
John <john.uko@live.com>
2022-07-01 20:30:30 -0700

    Two digit combinations program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print all possible combinations of two single digits in ascending order and separated by commas and spaces
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a !=8 || b !=9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
