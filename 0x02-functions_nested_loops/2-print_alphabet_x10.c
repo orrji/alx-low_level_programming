49a7040d7e4b3aa28b66429fb09142f92ae616f3
John <john.uko@live.com>
2022-07-05 14:27:38 -0700

    Alphabet x10 program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet 10x
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int r;
+	char c;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
