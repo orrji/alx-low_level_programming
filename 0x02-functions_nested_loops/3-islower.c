c5afd968f79733e69f777bbb72fa2c6f5c35aef1
John <john.uko@live.com>
2022-07-05 14:52:05 -0700

    Is lower program

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if c is lowercase
+ *
+ * Return: 1 if lowercase and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 'a' && c<= 'z')
+	{
+		_putchar(1);
+		else (0);
+	}
+	return (0);
+}
