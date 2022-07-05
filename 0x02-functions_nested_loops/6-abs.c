65accff8e07f5a3edcf27dd60fc03a491c7b9d4e
John <john.uko@live.com>
2022-07-05 16:09:05 -0700

    Absolute value program

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else 
+	{
+		return (-i);
+	}
+}
