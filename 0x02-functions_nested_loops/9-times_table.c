45a09e97fbe6b058f47e142d49add568beed5e6e
John <john.uko@live.com>
2022-07-05 21:20:03 -0700

    9 times refix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,10 @@ void times_table(void)
 
 	for (b = 0; b < 10; b++)
 	{
-		for (a = 1; a < 10; a++)
+		for (a = 0; a < 10; a++)
 		{
 			c = a * b;
-			if (c <= 9)
+			if (c <= 9 && c >= 0)
 			{
 				_putchar(',');
 				_putchar(' ');
