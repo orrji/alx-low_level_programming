c811333dbfaebc862e9c7a0f6e8ac20d13f6e1fc
John <john.uko@live.com>
2022-07-05 20:10:55 -0700

    Additon program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *@a - An integer input a
+ *@b - An integer input b
+ * Return: returns sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b
+
+		return (sum);
+}
