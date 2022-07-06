56015e4d86d7855ae8950903663335276e598182
John <john.uko@live.com>
2022-07-05 17:12:20 -0700

    Last digit fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
+ * print_last_digit - Prints last digit of number
  * @i: An integer input
- * Return: Last digit
+ * Return: Value of last digit
  */
 
 int print_last_digit(int i)
 {
 	int a;
 
-	if (i > 0)
-	{
-		a = i % 10;
-		_putchar(a + '0');
-	}
-	else
-	{
-		a = -1 * i % 10;
-		_putchar(a + '0');
-	}
+	if (i < 0)
+	i = -i;
+	a = i % 10
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
 	return (a);
-}
---
+++ 0x02-functions_nested_loops/7-print_last_digit3.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: An integer input
+ * Return: Last digit
+ */
+
+int print_last_digit(int i)
+{
+	int a;
+
+	if (i > 0)
+	{
+		a = i % 10;
+		_putchar(a + '0');
+	}
+	else
+	{
+		a = -1 * i % 10;
+		_putchar(a + '0');
+	}
+	return (a);
+}
