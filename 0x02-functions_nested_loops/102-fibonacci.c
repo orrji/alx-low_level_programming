3af1724c26038eba71359eedc0cb6e2d1640d52f
John <john.uko@live.com>
2022-07-05 21:37:47 -0700

    Fibonacci program

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 fibonacci numbers
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	int a;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (a = 2; a < 50; a++)
+	{
+		fibonacci[a] = fibonacci[a - 1] + fibonacci[a - 2];
+		if (a == 49)
+			printf("%ld\n", fibonacci[a]);
+		else
+			printf("%ld, ", fibonacci[a]);
+	}
+	return (0);
+}
