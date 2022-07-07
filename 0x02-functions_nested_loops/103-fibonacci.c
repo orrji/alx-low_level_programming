3313165722562d9a8694325af0dd0344c46647eb
John <john.uko@live.com>
2022-07-06 19:45:06 -0700

    Fibonacci sum program

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,18 @@
 
 int main(void)
 {
-	int i = 1, j = 2, sum = 0;
-	
-	while (i < 4000000)
+	long int i = 1, j = 2, k = 0, sum = 0;
+
+	while (k <= 4000000)
 	{
-		i = i + j;
-		j = i - j;
-	}
-	if (i % 2 == 0)
-	{
+		k = i + j;
+		i = j;
+		j = k;
+		if ((i % 2) == 0)
+		{
 		sum += i;
+		}
 	}
-	printf("%d\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
