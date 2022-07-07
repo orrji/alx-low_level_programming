c78df838da1f152976e59c064ec78be3789dd017
John <john.uko@live.com>
2022-07-06 18:25:00 -0700

    9Times fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (b = 0; b < 10; b++)
+	for (b = 0; b <= 9; b++)
 	{
-		for (a = 0; a < 10; a++)
+		for (a = 0; a <= 9; a++)
 		{
 			c = a * b;
 			if (c <= 9)
