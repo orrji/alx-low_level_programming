53705889b9d0315e22e6832c8120b7a966c76a3e
John <john.uko@live.com>
2022-07-06 19:56:56 -0700

    First 98 Fib numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - First 98 fibonacci numbers
+ *
+ * Return: Fib numbers
+ */
+
+int main(void)
+{
+	int i, n;
+
+	int s1 = 1, s2 = 2;
+
+	printf("%d, %d, ", s1, s2);
+	for (i = 3; i <= 96; i++)
+	{
+		printf("%d, ", nextTerm);
+		s1 = s2;
+		s2 = nextTerm;
+		nextTerm = s1 + s2;
+	}
+	return (0);
+}
