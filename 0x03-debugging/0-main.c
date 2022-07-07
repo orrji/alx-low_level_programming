5ad28159de6630bc014d0bad7d34adbb5fd76cd1
John U <john.uko@live.com>
2022-07-07 18:43:39 +0000

    multiple main code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if integer is +ve or -ve
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
