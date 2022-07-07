b59d070c65ecac7acd443d3adeae9541971870c3
John <john.uko@live.com>
2022-07-06 18:15:18 -0700

    9Times fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,12 @@ void times_table(void)
 		for (a = 0; a < 10; a++)
 		{
 			c = a * b;
-			if (c < 9 && b < 9)
+			if (c < 9)
 			{
-				_putchar(c + '0');
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-			}
-			else if (c == 9)
-			{
-				_putchar(c);
-				_putchar(',');
-				_putchar(' ');
+				_putchar(c + '10');
 			}
 			else
 			{
