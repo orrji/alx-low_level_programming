9ff92dd7da1a2ae5d4d3716d5015faf36221f576
John U <john.uko@live.com>
2022-07-08 01:55:56 +0000

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
