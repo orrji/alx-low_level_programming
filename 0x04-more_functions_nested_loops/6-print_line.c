1809f460c05f60beeb5da49955b7095effa106a7
John U <john.uko@live.com>
2022-07-08 03:14:56 +0000

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@ void print_line(int n)
 {
 	int a;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		for (a = 0; a < n; a++)
 		{
-			_putchar(95);
+			_putchar('_');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
