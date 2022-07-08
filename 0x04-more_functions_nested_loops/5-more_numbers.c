f3e2b1b53b1c672b701ec48d224f5af65697e1da
John U <john.uko@live.com>
2022-07-08 02:08:03 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints more numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 < 15; n2++)
+		{
+			if (n >= 10)
+			{
+				_putchar((n2 / 10) + '0');
+				_putchar((n2 % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
