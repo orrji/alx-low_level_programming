766a8fe97465dc9352b343759ca47abd25ab68a5
John U <john.uko@live.com>
2022-07-11 22:34:02 +0000

    Task 2 - String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns length of a string
+ * @s: string parameter
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
