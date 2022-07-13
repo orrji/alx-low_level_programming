87cfed47e58a871ce22f0ee94016603f526a3594
John U <john.uko@live.com>
2022-07-13 23:03:14 +0000

    Task 7 - Encode string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string
+ * @s: string parameter
+ * Return: Encoded integer
+ */
+
+char *leet(char *s);
+{
+	int i1, i2;
+	char c1[] = "aAeEoOtTlL";
+	char c2[] = "4433007711";
+
+	for (i1 = 0; *(s + i1); i1++)
+	{
+		for (i2 = 0; i2 <= 9; i2++)
+		{
+			if (c1[i2] == *(s + i1))
+				*(s + i1) = c2[i2];
+		}
+	}
+	return (s);
+}
