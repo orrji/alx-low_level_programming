9f402042b76256152dacf6cbfef82b7943a0f48e
John U <john.uko@live.com>
2022-07-13 22:40:23 +0000

    Task 5 - Lower to Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase to uppercase
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int v1 = 0;
+
+	while (*(s + v1))
+	{
+		if (*(s + v1) >= 'a' && *(s + v1) <= 'z')
+			*(s + v1) -= 'a' - 'A';
+		v1++;
+	}
+	return (s);
+}
