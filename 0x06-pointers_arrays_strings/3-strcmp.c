461c55d9125a8ef5798e192ac45f4b0841564aa4
John U <john.uko@live.com>
2022-07-13 21:55:50 +0000

    Task 3 - Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: first string parameter
+ * @s2: second string parameter
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+}
