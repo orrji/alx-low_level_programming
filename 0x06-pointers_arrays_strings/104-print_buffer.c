9db93f19a42c3dfeea7e6cfc67dd87600e6cff16
John U <john.uko@live.com>
2022-07-14 00:47:46 +0000

    Task 104 - Print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints buffer
+ * @b: character input
+ * @size: size integer
+ * Return: buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+
