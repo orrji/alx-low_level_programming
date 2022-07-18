683c60b38b90b1e5995c32e5bc98bb5a5db75e85
John U <john.uko@live.com>
2022-07-18 21:39:23 +0000

    Task 0 - Constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with constant byte
+ * @s: pointer parameter
+ * @b: character parameter
+ * @n: Integer parameter
+ * Return: Pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s +i) = b;
+	}
+	return (s);
+}
