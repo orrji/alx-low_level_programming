14589793198513d7526a390c0a7dc95b4cdd275f
John U <john.uko@live.com>
2022-07-20 21:34:47 +0000

    Task 4 - exponent calculation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates value of x to the power y
+ * @x: base integer parameter
+ * @y: power integer parameter
+ * Return: power calculated or -1 if y<0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
