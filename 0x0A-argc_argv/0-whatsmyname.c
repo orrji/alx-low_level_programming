2c9d394ebff4135dc169e586e98cd1607ba19f72
John U <john.uko@live.com>
2022-07-23 00:58:29 +0000

    Task 0 - name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of a program followed by a new line
+ * @argc: array size
+ * @argv: array containing compile argument
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0)
+}
